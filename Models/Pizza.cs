8f4cfdb5ba39a4f62afb3ba810abb135d6a03347
IGNACIO MATIAS LOPEZ ABRAHAM <46872351@ort>
2023-03-10 12:02:53 -0300

    added pizza.cs

---
+++ Models/Pizza.cs
@@ -0,0 +1,19 @@
+Using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace Pizzas.Models;
+
+public class Pizza{
+    private int _id, _precio;
+    private string _nombre, descripcion;
+
+    public Pizza(){}
+
+    public Pizza(){
+        
+    }
+}
